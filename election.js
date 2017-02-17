8ba70991b3b33a5310ca55c75d27485debdcd43d
doctor3397 <hs2546@columbia.edu>
2017-02-17 13:03:25 -0500

    Add candidates

--- election.js
+++ election.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method:'GET'
+
+  }).done(function(data){
+    console.log(data);
+
+    for (var i=0; i < data.candidates.length; i++){
+
+      // $('<h3>').html(data.candidates[i].id).appendTo('#candidates');
+      // $('<li>').html(data.candidates[i].name).appendTo('#candidates');
+      // $('<li>').html(ata.candidates[i].votes).appendTo('#candidates');
+
+      $('#candidates').append(
+        '<h3> Cadidate: ' + data.candidates[i].id + '</h3>' +
+        '<li>' + data.candidates[i].name + '</li><br>' +
+        '<li>' + data.candidates[i].votes + '</li>'
+      );
+    }
+
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    console.log('Ajax Request Failed');
+    console.log(jqXHR);
+  }).always(function(){
+    console.log('Ajax Request Sent');
+  });
 
 });
